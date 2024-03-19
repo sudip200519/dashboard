d4e9f60ece85d78ee799551b0ff2ba0ca5d5b295
sudip200519 <163457230+sudip200519@users.noreply.github.com>
2024-03-19 16:15:26 +0530

    Update admin.py

--- ezmall/Inv/admin.py
+++ ezmall/Inv/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Courses)
+admin.site.register(Buy)
+admin.site.register(Tags)
+
+
