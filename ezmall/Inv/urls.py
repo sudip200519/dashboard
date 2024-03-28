204ded0e9128f92a9ca90e094cd22f0baa64b67d
sudip200519 <163457230+sudip200519@users.noreply.github.com>
2024-03-28 12:13:54 +0530

    Update urls.py

--- ezmall/Inv/urls.py
+++ ezmall/Inv/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .import views
 
-urlpatterns = [
-    path('',views.dashboard,name='Home')
+urlpatterns =[
+    path('', views.dashboard,name='Home'),
+    path('courses/', views.Courses,name='Courses'),
+    path('student/<str:pk_test>/',views.Student,name='Student'),
+    path('', views.index),
+    path('add/', views.add_person),
+    path('<path:remaining_url>/', views.catch_all_view),
+    path('Inv/',views.index, name="Inv"),
+    path('show/',views.get_all_person)
 ]
